d11e7d8ae54512de5b62428a3aa0b5e026baf51e
WillianXG <wilian.mine789@gmail.com>
2024-02-22 10:23:16 -0300

    error slider

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import styles from './NavBar.module.css';
+// import React, { useState } from 'react';
+// import styles from './NavBar.module.css';
 
-import Sliders from '../slider/slider';
+// import Sliders from '../slider/slider';
 
-function NavBar() {
-    const [isActive, setIsActive] = useState(false);
+// function NavBar() {
+//     const [isActive, setIsActive] = useState(false);
 
-    const toggleNav = () => {
-        setIsActive(!isActive);
-    };
+//     const toggleNav = () => {
+//         setIsActive(!isActive);
+//     };
 
-    return (
-        <>
-            <header className={styles.header}>
-                <nav className={`${styles.nav} ${isActive ? styles.active : ''}`}>
-                    <a href='/' className={styles.logo}><h1>Psicóloga Ana Célia do Nascimento</h1></a>
-                    <button className={styles.hamburger} onClick={toggleNav}></button>
-                    <ul className={styles.navList}>
-                        <li><a href='#' className={styles.navBtn}>Inicio</a></li>
-                        <li><a href='#' className={styles.navBtn}>Sobre</a></li>
-                        <li><a href='#' className={styles.navBtn}>Tratamentos</a></li>
-                        <li><a href='#' className={styles.navBtn}>Contato</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main className={styles.hero}>
-                <Sliders></Sliders>
-            </main>
-        </>
-    );
-}
+//     return (
+//         <>
+//             <header className={styles.header}>
+//                 <nav className={`${styles.nav} ${isActive ? styles.active : ''}`}>
+//                     <a href='/' className={styles.logo}><h1>Psicóloga Ana Célia do Nascimento</h1></a>
+//                     <button className={styles.hamburger} onClick={toggleNav}></button>
+//                     <ul className={styles.navList}>
+//                         <li><a href='#' className={styles.navBtn}>Inicio</a></li>
+//                         <li><a href='#' className={styles.navBtn}>Sobre</a></li>
+//                         <li><a href='#' className={styles.navBtn}>Tratamentos</a></li>
+//                         <li><a href='#' className={styles.navBtn}>Contato</a></li>
+//                     </ul>
+//                 </nav>
+//             </header>
+//             <main className={styles.hero}>
+//                 <Sliders></Sliders>
+//             </main>
+//         </>
+//     );
+// }
 
-export default NavBar;
+// export default NavBar;
