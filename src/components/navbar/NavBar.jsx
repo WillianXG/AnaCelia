33d482672fb7ce9fe6ac38d4f629a4e934c01c8f
WillianXG <wilian.mine789@gmail.com>
2024-02-21 23:28:04 -0300

    hambuguer navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,12 +1,19 @@
-import styles from './NavBar.module.css'
+import React, { useState } from 'react';
+import styles from './NavBar.module.css';
 
+function NavBar() {
+    const [isActive, setIsActive] = useState(false);
 
-function NavBar(){
-    return(
+    const toggleNav = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
         <>
             <header className={styles.header}>
-                <nav className={styles.nav}>
+                <nav className={`${styles.nav} ${isActive ? styles.active : ''}`}>
                     <a href='/' className={styles.logo}><h1>Psicóloga Ana Célia do Nascimento</h1></a>
+                    <button className={styles.hamburger} onClick={toggleNav}></button>
                     <ul className={styles.navList}>
                         <li><a href='#' className={styles.navBtn}>Inicio</a></li>
                         <li><a href='#' className={styles.navBtn}>Sobre</a></li>
@@ -16,8 +23,8 @@ function NavBar(){
                 </nav>
             </header>
             <main className={styles.hero}></main>
-        </>    
-        )
+        </>
+    );
 }
 
-export default NavBar
+export default NavBar;
