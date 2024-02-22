e1453ffe804d938042322efd2c13b8ce23c0fbf7
WillianXG <wilian.mine789@gmail.com>
2024-02-22 10:26:59 -0300

    error slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@
 import React from 'react';
 import NavBar from './components/navbar/NavBar';
 import './App.css'
-import { register } from 'swiper/element/bundle'
 
-register();
-  import 'swiper/css';
-  import 'swiper/css/pagination';
-  import 'swiper/css/scrollbar'
 
 
 function App() {
