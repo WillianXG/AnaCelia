f9ef59db8662ef0b2abc0d631c60985b28c54455
WillianXG <wilian.mine789@gmail.com>
2024-04-05 12:00:52 -0300

    Header

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,11 +4,15 @@ import Main from "../../components/Corpo/main";
 import styles from './home.module.css'
 import Footer from "../../components/Footer/footer";
 import Carrosel from "../../components/Carrossel/carrossel";
+import HDER from "../../components/Header/header";
 
 function Home(){
     return(
         <>
             <NavBar/>
+            <br/>
+            <br/>
+            <HDER/>
             <br/>
             <br/>
             <br/>
