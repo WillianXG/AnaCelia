761ef5ee5f74f9ca6093d620fcfce312e23ea353
WillianXG <wilian.mine789@gmail.com>
2024-06-14 17:06:17 -0300

    button repair

--- src/components/mainSobre/mainSobre.jsx
+++ src/components/mainSobre/mainSobre.jsx
@@ -3,9 +3,9 @@ import styles from "./mainSobre.module.css";
 
 function MainSobre() {
   const [info, setInfo] = useState({
-    horario: "⏰ Horário de Funcionamento: Segunda a Sexta: 8h às 20h Sábado: 9h às 14h⏰",
+    horario: "⏰Horário de Funcionamento: Segunda a Sexta: 9h às 21h Sábado: 8h às 13h⏰",
     mapaSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.7094753716933!2d-43.28480698827908!3d-22.88718817918243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997d149a1058f7%3A0x74aaf0a5b36f3684!2sTorre%20Norte%20Shopping!5e0!3m2!1spt-BR!2sbr!4v1712684015097!5m2!1spt-BR!2sbr",
-    endereco : "Av. Dom Hélder Câmara, 5200 - Cachambi, Rio de Janeiro - RJ, CEP 20771-004"
+    endereco : "Av. Dom Hélder Câmara, 5200 - Cachambi, Rio de Janeiro CEP 20771-004 - Sala 925"
   });
 
   const handleButtonClick = (horario, mapaSrc, endereco) => {
